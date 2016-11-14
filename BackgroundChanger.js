25667e695df0e4453bc4cf647c524736e0f04613
Amin Arjmand <aminarj2000@gmail.com>
2016-11-14 10:43:18 +0330

    Add files via upload

---
+++ BackgroundChanger.js
@@ -0,0 +1,19 @@
+//Generate Random Number
+function rand(start, end) {
+    var r = start + Math.floor(Math.random() * (end - start));
+    return r;
+}
+
+//Some Style For Button
+document.body.style.textAlign = "center";
+document.body.style.marginTop = "100px";
+
+//Random Background Color Generator Function
+function backgroundChanger() {
+    //background Color For Body
+    var bgColor = "rgb(" + rand(0, 255) + "," + rand(0, 255) + "," + rand(0, 255) + ")";
+    //background Color For Texts
+    var bgColor2 = "rgb(" + rand(0, 255) + "," + rand(0, 255) + "," + rand(0, 255) + ")";
+    document.body.style.backgroundColor = bgColor;
+    document.body.style.color = bgColor2;
+}
